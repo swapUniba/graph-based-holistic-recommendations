e033777ce3ffebec7e5b64f2165e6a29c029b380
federicoimpellizzeri <f.impellizzeri1@studenti.uniba.it>
2019-02-13 18:39:34 +0100

    Update jung folder (to be sure)

--- src/main/java/jung/Main.java
+++ src/main/java/jung/Main.java
@@ -10,7 +10,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-
         //TOTEST WITH AND WITHOUT P_
         String citta = "torino"; //"bari"
         //int numero_persone = 100;
@@ -34,84 +33,9 @@ public class Main {
         ArrayList<Boolean> connection_type = new ArrayList<Boolean>();
         connection_type.add(false);
         connection_type.add(true);
-        try (PrintWriter writer = new PrintWriter(new File("result_humgraph.csv"))) {
 
-            StringBuilder sb = new StringBuilder();
-            sb.append("company");
-            sb.append(",");
-            sb.append("type");
-            sb.append(',');
-            sb.append("mood");
-            sb.append(',');
-            sb.append("health");
-            sb.append(',');
-            sb.append("settimana");
-            sb.append(',');
-            sb.append("city");
-            sb.append(",");
-            sb.append("cardinality user");
-            sb.append(",");
-            sb.append("full connection");
-            sb.append(",");
-            sb.append("top_number");
-            sb.append(",");
-            sb.append("alg");
-            for (int n = 0; n < top_risultati; n++) {
-                sb.append(",");
-                sb.append("Top" + String.valueOf(n + 1) + "Name");
-                sb.append(',');
-                sb.append("Top" + String.valueOf(n + 1) + "Score");
-            }
-            sb.append('\n');
-            writer.write(sb.toString());
-
-            System.out.println("done!");
-
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-        for (int cc = 0; cc < cities.size(); cc++) {
-            String city = cities.get(cc);
-            for (int cn = 0; cn < num_users.size(); cn++) {
-                Integer num = num_users.get(cn);
-                for (int c_cn = 0; c_cn < connection_type.size(); c_cn++) {
-                    Boolean type = connection_type.get(c_cn);
-                    System.out.println("------------------------ EXPERIMENT DETAILS ------------------------ ");
-                    System.out.println(contesto);
-                    System.out.println(city + "\t" + String.valueOf(num) + "\t" + String.valueOf(type));
-                    System.out.println("-------------------------------------------------------------------- ");
-                    String conf = "";
-                    for (int cl = 1; cl < contesto.size(); cl++) {
-                        conf += String.valueOf(contesto.get(cl)) + ',';
-                    }
-
-                    conf += city + ',' + String.valueOf(num) + ',' + String.valueOf(type) + ',' + String.valueOf(top_risultati)+',';
-
-                    Grafo g = new Grafo(city, num, type, contesto);
-                    HashMap<String, Double> alg1 = g.Pagerank(top_risultati);
-                    HashMap<String, Double> alg2 = g.PagerankPriors(top_risultati);
-                    String pr = "";
-                    pr += "PageRank";
-                    for (Map.Entry<String, Double> entry : alg1.entrySet()) {
-                        String key = entry.getKey();
-                        Double value = entry.getValue();
-                        pr += ',' + key + ',' + value;
-                    }
-                    g.addConfiguration("result_humgraph.csv", conf + pr);
-
-                    String prp = "";
-                    prp += "PageRankPriors";
-                    for (Map.Entry<String, Double> entry : alg2.entrySet()) {
-                        String key = entry.getKey();
-                        Double value = entry.getValue();
-                        prp += ',' + key + ',' + value;
-                    }
-
-                    g.addConfiguration("result_humgraph.csv", conf + prp);
-                    System.out.println("\n");
-                }
-            }
-        }
+        Experiment set_exps = new Experiment();
+        set_exps.runExperiments("result_hum.csv", cities, num_users, connection_type, contesto, top_risultati);
 
     }
 
