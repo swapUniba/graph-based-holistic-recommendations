8ff2c26078f336c8bf8f5a64262569b60e33c54e
federicoimpellizzeri <f.impellizzeri1@studenti.uniba.it>
2019-02-14 00:46:02 +0100

    Generate More Context (in order to have more data)

--- src/main/java/jung/Main.java
+++ src/main/java/jung/Main.java
@@ -10,13 +10,14 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
 
+        int number_context_randomgenerated = 500;
         //TOTEST WITH AND WITHOUT P_
         String citta = "torino"; //"bari"
         //int numero_persone = 100;
         boolean full_connected = false;
         int top_risultati = 5;
         //List<String> contesto = Arrays.asList("P_0", "C_bambini","C_colazione","C_cattivo_umore","C_non_buona_salute","C_weekend");
-        List<String> contesto = FromFile.RandomContext();
+        //List<String> contesto = FromFile.RandomContext();
         //Grafo grafo = new Grafo(citta, numero_persone, full_connected, contesto);
         //grafo.Pagerank(top_risultati);
         //grafo.PagerankPriors(top_risultati);
@@ -26,16 +27,18 @@ public class Main {
         cities.add("bari");
         cities.add("torino");
         ArrayList<Integer> num_users = new ArrayList<Integer>();
-        num_users.add(1);
-        num_users.add(100);
-        num_users.add(500);
-        num_users.add(1000);
+        for (int i = 1; i <= 1000; i = i + 200) {
+            num_users.add(i);
+        }
         ArrayList<Boolean> connection_type = new ArrayList<Boolean>();
         connection_type.add(false);
         connection_type.add(true);
 
         Experiment set_exps = new Experiment();
-        set_exps.runExperiments("result_hum.csv", cities, num_users, connection_type, contesto, top_risultati);
+        for (int k_c = 0; k_c < number_context_randomgenerated; k_c++){
+            List<String> contesto = FromFile.RandomContext();
+            set_exps.runExperiments("result_hum1.csv", cities, num_users, connection_type, contesto, top_risultati);
+        }
 
     }
 
