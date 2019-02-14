bc3117752475cf9ea2de13321a988870a54d8dc2
Marco Mirizio <damn.miri@gmail.com>
2019-02-14 03:04:19 +0100

    Revert "Full e NotFull Connected implementato"
    This reverts commit 0a6615fa

--- src/main/java/jung/Main.java
+++ src/main/java/jung/Main.java
@@ -7,18 +7,22 @@ import java.util.List;
 public class Main {
     public static void main( String[] args ) throws IOException {
 
-        //TOTEST WITH AND WITHOUT P_
-        String citta = "torino"; //"bari"
-        int numero_persone = 3;
-        boolean full_connected = false;
-        int top_risultati = 5;
-        //List<String> contesto = Arrays.asList("P_0", "C_bambini","C_colazione","C_cattivo_umore","C_non_buona_salute","C_weekend");
-        List<String> contesto = FromFile.RandomContext();
+        int numero_persone=1;
 
-        Grafo grafo = new Grafo(citta, numero_persone, full_connected, contesto);
+        Grafo grafo = new Grafo("torino", numero_persone);
+        //Grafo grafo = new Grafo("bari", numero_persone);
+
+
+
+
+        //TOTEST WITH AND WITHOUT P_
+        int top_risultati =5;
         //grafo.Pagerank(top_risultati);
-        grafo.PagerankPriors(top_risultati);
+        //List<String> contesto = Arrays.asList("P_0", "C_bambini","C_colazione","C_cattivo_umore","C_non_buona_salute","C_weekend");
+        //grafo.PagerankPriors(contesto, top_risultati);
+        grafo.PagerankPriors(FromFile.RandomContext(), top_risultati);
 
         grafo.Mostra();
+
     }
 }
