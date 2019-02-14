8cb80fa91d87ab293e84416adf92ea10b38360a8
federicoimpellizzeri <f.impellizzeri1@studenti.uniba.it>
2019-02-14 21:12:26 +0100

    Create Logs File - quasi finito

--- src/main/java/jung/Main.java
+++ src/main/java/jung/Main.java
@@ -15,9 +15,9 @@ public class Main {
         String citta = "torino"; //"bari"
         //int numero_persone = 100;
         boolean full_connected = false;
-        int top_risultati = 5;
+        int top_risultati = 3;
         //List<String> contesto = Arrays.asList("P_0", "C_bambini","C_colazione","C_cattivo_umore","C_non_buona_salute","C_weekend");
-        //List<String> contesto = FromFile.RandomContext();
+        List<String> contesto = FromFile.RandomContext();
         //Grafo grafo = new Grafo(citta, numero_persone, full_connected, contesto);
         //grafo.Pagerank(top_risultati);
         //grafo.PagerankPriors(top_risultati);
@@ -27,18 +27,22 @@ public class Main {
         cities.add("bari");
         cities.add("torino");
         ArrayList<Integer> num_users = new ArrayList<Integer>();
-        for (int i = 1; i <= 1000; i = i + 200) {
-            num_users.add(i);
-        }
+        num_users.add(1);
+        //num_users.add(20);
+//        for (int i = 1; i <= 1000; i = i + 200) {
+//            num_users.add(i);
+//        }
         ArrayList<Boolean> connection_type = new ArrayList<Boolean>();
         connection_type.add(false);
-        connection_type.add(true);
+        //connection_type.add(true);
 
         Experiment set_exps = new Experiment();
-        for (int k_c = 0; k_c < number_context_randomgenerated; k_c++){
-            List<String> contesto = FromFile.RandomContext();
-            set_exps.runExperiments("result_hum1.csv", cities, num_users, connection_type, contesto, top_risultati);
-        }
+        set_exps.runExperiments("simulazione.csv", cities, num_users, connection_type, contesto, top_risultati);
+
+//        for (int k_c = 0; k_c < number_context_randomgenerated; k_c++){
+//            List<String> contesto = FromFile.RandomContext();
+//            set_exps.runExperiments("result_hum1.csv", cities, num_users, connection_type, contesto, top_risultati);
+//        }
 
     }
 
